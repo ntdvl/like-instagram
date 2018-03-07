37b6907904e05402303163509f99c004e8049a23
ntdvl <yilmazkoman@outlook.com>
2018-03-07 20:40:47 +0300

    added fontello

---
+++ app.js
@@ -0,0 +1,9 @@
+var x = document.querySelector('.item > .test');
+
+var y = document.querySelector('.demo-icon');
+
+x.addEventListener('dblclick', function() {
+
+	y.style.color= 'red';
+
+}, false);
